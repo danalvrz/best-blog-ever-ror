c6e168a0364420a6eba2934311c08d2835e9f523
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-04-04 16:42:00 -0500

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-    belongs_to :post, class_name: 'Post', foreign_key: :post_id
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-    def update_comment_counter(id)
-        new_counter = Comment.where(post_id: id).count
-        Post.where(id: id)update(comments_counter: new_counter)
-    end
+  def update_comment_counter(id)
+    new_counter = Comment.where(post_id: id).count
+    Post.where(id: id).update(comments_counter: new_counter)
+  end
 end
 
 # first_post = Post.create(author: User.find_by(id: 3), title: 'Hello', text: 'This is my first post')
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,9 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    belongs_to :post, class_name: 'User', foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, class_name: 'User', foreign_key: :post_id
 
-    def update_like_counter(id)
-        new_counter = Like.where(post_id: id).count
-        Post.where(id: id).update(likes_counter: new_counter)
-    end
+  def update_like_counter(id)
+    new_counter = Like.where(post_id: id).count
+    Post.where(id: id).update(likes_counter: new_counter)
+  end
 end
