dd70d30f23d340577012f7fb4dc9504bcc69f187
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-04-04 17:31:41 -0500

    Delete testing comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,15 +7,3 @@ class Comment < ApplicationRecord
     Post.where(id: id).update(comments_counter: new_counter)
   end
 end
-
-# first_post = Post.create(author: User.find_by(id: 3), title: 'Hello', text: 'This is my first post')
-# second_post = Post.create(author: User.find_by(id: 3), title: 'Hi', text: 'This is my second post')
-# third_post = Post.create(author: User.find_by(id: 4), title: 'Goodbye', text: 'This is my third post')
-# fourth_post = Post.create(author: User.find_by(id: 4), title: 'Ciao', text: 'This is my fourth post')
-
-# Comment.create(post: Post.find_by(id: 1), author: User.find_by(id: 3), text: 'Hi Tom!')
-# Comment.create(post: Post.find_by(id: 1), author: User.find_by(id: 4), text: 'Hi Lilly!')
-# Comment.create(post: Post.find_by(id: 1), author: User.find_by(id: 3), text: 'How are you, Tom?')
-# Comment.create(post: Post.find_by(id: 1), author: User.find_by(id: 4), text: 'Good, and you?')
-# Comment.create(post: Post.find_by(id: 1), author: User.find_by(id: 3), text: 'Great!')
-# Comment.create(post: Post.find_by(id: 1), author: User.find_by(id: 4), text: 'Sweet!')
