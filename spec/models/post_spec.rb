bad69a86db68e3663c41acf60e36db3b18e55b46
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-04-08 14:30:30 -0500

    Fix linter errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,36 +1,37 @@
 require_relative '../rails_helper'
 
 RSpec.describe Post, type: :model do
-    author = User.new(name: "Tom")
-    subject { Post.new(title: "Star Wars", author_id: author.id, text: "This is a test", comments_counter: 1, likes_counter: 1) }
+  author = User.new(name: 'Tom')
+  subject do
+    Post.new(title: 'Star Wars', author_id: author.id, text: 'This is a test', comments_counter: 1, likes_counter: 1)
+  end
 
-    before(:each) { subject.save }
+  before(:each) { subject.save }
 
-    it 'title has to be present' do
-        subject.title = nil
-        expect(subject).to_not be_valid
-    end
+  it 'title has to be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
 
-    it 'saves correct author' do
-        expect(subject.author_id).to equal(author.id)
-    end
+  it 'saves correct author' do
+    expect(subject.author_id).to equal(author.id)
+  end
 
-    it 'text is no longer that 250' do
-        subject.text = 'test' * 45
-        expect(subject).to_not be_valid
-    end
+  it 'text is no longer that 250' do
+    subject.text = 'test' * 45
+    expect(subject).to_not be_valid
+  end
 
-    it 'comments_counter should be a number' do
-        expect(subject.comments_counter).to be(1)
-    end
+  it 'comments_counter should be a number' do
+    expect(subject.comments_counter).to be(1)
+  end
 
-    it 'likes_counter should be a number' do
-        expect(subject.comments_counter).to be(1)
-    end
+  it 'likes_counter should be a number' do
+    expect(subject.comments_counter).to be(1)
+  end
 
-    it 'posts_counter updates authors counter' do
-        author.increment!(:posts_counter)
-        expect(author.posts_counter).to be(1)
-    end
-
+  it 'posts_counter updates authors counter' do
+    author.increment!(:posts_counter)
+    expect(author.posts_counter).to be(1)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,26 @@
 require_relative '../rails_helper'
 
 RSpec.describe User, type: :model do
-    subject { User.new(name: "Jenny", posts_counter: 8) }
+  subject { User.new(name: 'Jenny', posts_counter: 8) }
 
-    before(:each) { subject.save }
+  before(:each) { subject.save }
 
-    it 'name has to be present' do
-        subject.name = nil
-        expect(subject).to_not be_valid
-    end
+  it 'name has to be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 
-    it 'posts_counter should be an number' do
-        subject.posts_counter = "xyz"
-        expect(subject).to_not be_valid
-    end
+  it 'posts_counter should be an number' do
+    subject.posts_counter = 'xyz'
+    expect(subject).to_not be_valid
+  end
 
-    it 'posts_counter should be greater than or equal to 0' do
-        subject.posts_counter = -8
-        expect(subject).to_not be_valid
-    end
+  it 'posts_counter should be greater than or equal to 0' do
+    subject.posts_counter = -8
+    expect(subject).to_not be_valid
+  end
 
-    it 'recet_posts should return 0 for new users' do
-        expect(subject.recent_posts.count).to be(0)
-    end
-    
+  it 'recet_posts should return 0 for new users' do
+    expect(subject.recent_posts.count).to be(0)
+  end
 end
