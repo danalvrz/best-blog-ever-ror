eede2fac4729e312d311d04d5dbebe5abb64b9aa
Oliver <olivercoimbra@gmail.com>
2022-04-13 10:56:08 -0400

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   def init
     self.posts_counter = 0
   end
-    # Include default devise modules. Others available are:
+  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
